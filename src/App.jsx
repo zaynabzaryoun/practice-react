4a21fde159a24eb769008f039334328227b4c46b
zaynabzaryoun <33anaab@gmail.com>
2025-02-20 23:54:03 +0330

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
+import Button from "./Button"
 import "./App.css"
-
-function MyButton() {
-  return (
-    <button style={{ color: 'blue' }}>im a button!</button>
-  )
-}
-
 
 function App() {
 
- 
+  const user = {
+    name: "sara", 
+    lasrname: "nazari"
+ }
 
   return (
     <>
-      <h1 className="color">hello , welcome to my app</h1>
-    <MyButton />
+      <h1 className="heading">hello , welcome to my app</h1>
+      <p>{"user name: " + user.name}</p>
+      <Button />
     </>
   )
 }
